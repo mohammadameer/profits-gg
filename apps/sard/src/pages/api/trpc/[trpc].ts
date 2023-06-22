af16bd7985b10a6c3011a001695c63f1994e1a82
Mohammad <mohammadameerabdallah@gmail.com>
2023-06-22 22:50:40 +0300

    remove batching

--- apps/sard/src/pages/api/trpc/[trpc].ts
+++ apps/sard/src/pages/api/trpc/[trpc].ts
@@ -26,4 +26,7 @@ export default createNextApiHandler({
           );
         }
       : undefined,
+  batching: {
+    enabled: false,
+  },
 });
--- apps/sard/src/utils/api.ts
+++ apps/sard/src/utils/api.ts
@@ -39,7 +39,7 @@ export const api = createTRPCNext<AppRouter>({
             process.env.NODE_ENV === "development" ||
             (opts.direction === "down" && opts.result instanceof Error),
         }),
-        httpBatchLink({
+        httpLink({
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
