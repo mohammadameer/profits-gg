8b1cd940c083707368f99c8fec33f1b3b589fab6
Mohammad <mohammadameerabdallah@gmail.com>
2023-06-23 20:14:50 +0300

    return tag manager and tiktok

--- apps/sard/src/pages/_app.tsx
+++ apps/sard/src/pages/_app.tsx
@@ -13,7 +13,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { IBM_Plex_Sans_Arabic } from "next/font/google";
-import Script from "next/script";
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== "undefined") {
@@ -47,34 +46,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
   }, []);
   return (
     <>
-      {router?.pathname != "/" ? (
-        <>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=AW-10865811504"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-            <!-- Google tag (gtag.js) -->
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            
-            gtag('config', 'AW-10865811504');
-          `}
-          </Script>
-          <Script strategy="afterInteractive">
-            {`
-            !function (w, d, t) {
-              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
-
-              ttq.load('CI1LOKRC77U4209V8L3G');
-              ttq.page();
-            }(window, document, 'ttq');
-          `}
-          </Script>
-        </>
-      ) : null}
       <PostHogProvider client={posthog}>
         <Layout>
           {/* <SessionProvider session={session}> */}
--- apps/sard/src/pages/_document.tsx
+++ apps/sard/src/pages/_document.tsx
@@ -1,9 +1,36 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html dir="rtl" lang="ar">
-      <Head></Head>
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10865811504"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            <!-- Google tag (gtag.js) -->
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'AW-10865811504');
+          `}
+        </Script>
+        <Script strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++
+        )ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=i+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+            
+              ttq.load('CIASNVJC77U7QBTMS4PG');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
